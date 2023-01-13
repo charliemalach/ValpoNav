4b2d7ec5a4099ab018c06191cbc11b12a21c06e5
Charlie Malachinski <87348126+charliemalach@users.noreply.github.com>
2023-01-13 14:38:18 -0600

    Added Buildings
    off-campus:
    dominos. panera, hungry howies, jimmy johns, cvs phar,.

--- map.js
+++ map.js
@@ -72,7 +72,27 @@ var buildings = [{
     name: 'Dominos Pizza',
     lat: 41.46656, 
     lng: -87.03962,
-    link: 'https://www.example1.com'
+    link: 'https://www.dominos.com/en/'
+}, {
+    name: 'Panera Bread',
+    lat: 41.46811, 
+    lng: -87.0275,
+    link: 'https://www.panerabread.com/en-us/home.html'
+}, {
+    name: 'Hungry Howies',
+    lat: 41.46856, 
+    lng: -87.0468,
+    link: 'https://www.hungryhowies.com/'
+}, {
+    name: 'Jimmy Johns',
+    lat: 41.46749, 
+    lng: -87.04097,
+    link: 'https://www.jimmyjohns.com/'
+}, {
+    name: 'CVS Pharmacy',
+    lat: 41.45974,  
+    lng: -87.06254,
+    link: 'https://www.cvs.com/'
 }
 
 ];
