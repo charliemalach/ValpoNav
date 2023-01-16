e0f956b110c1723212654b0ed52e7a805ced900c
jklayhew <83787420+jklayhew@users.noreply.github.com>
2023-01-16 17:14:16 -0500

    fixed buildings not being centered witin bounds

--- map.js
+++ map.js
@@ -8,49 +8,59 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Create an array of building objects that includes the building name and location
 var buildings = [{
-        name: 'Christopher Center',
-        lat: 41.46268,
-        lng: -87.04299,
-        link: 'https://www.example1.com'
-    }, {
-        name: 'Harre Union',
-        lat: 41.46421,
-        lng: -87.04226
-    }, {
-        name: 'Weseman Hall',
-        lat: 41.46154,
-        lng: -87.05325,
-        link: 'https://www.example1.com'
-    }, {
-        name: 'Neils Science Center',
-        lat: 41.46482,
-        lng: -87.04059,
-        link: 'https://www.example1.com'
-    }, {
-        name: 'Dominos Pizza',
-        lat: 41.46656,
-        lng: -87.03962,
-        link: 'https://www.dominos.com/en/'
-    }, {
-        name: 'Panera Bread',
-        lat: 41.46811,
-        lng: -87.0275,
-        link: 'https://www.panerabread.com/en-us/home.html'
-    }, {
-        name: 'Hungry Howies',
-        lat: 41.46856,
-        lng: -87.0468,
-        link: 'https://www.hungryhowies.com/'
-    }, {
-        name: 'Jimmy Johns',
-        lat: 41.46749,
-        lng: -87.04097,
-        link: 'https://www.jimmyjohns.com/'
-    }, {
-        name: 'CVS Pharmacy',
-        lat: 41.45974,
-        lng: -87.06254,
-        link: 'https://www.cvs.com/'
-    }
+    name: 'Christopher Center',
+    lat: 41.46268,
+    lng: -87.04299,
+    link: 'https://www.example1.com'
+}, {
+    name: 'Harre Union',
+    lat: 41.46421,
+    lng: -87.04226
+}, {
+    name: 'Weseman Hall',
+    lat: 41.46154,
+    lng: -87.05325,
+    link: 'https://www.example1.com'
+}, {
+    name: 'Neils Science Center',
+    lat: 41.46482,
+    lng: -87.04059,
+    link: 'https://www.example1.com'
+}, {
+    name: 'Dominos Pizza',
+    lat: 41.46656,
+    lng: -87.03962,
+    link: 'https://www.dominos.com/en/'
+}, {
+    name: 'Panera Bread',
+    lat: 41.46811,
+    lng: -87.0275,
+    link: 'https://www.panerabread.com/en-us/home.html'
+}, {
+    name: 'Hungry Howies',
+    lat: 41.46856,
+    lng: -87.0468,
+    link: 'https://www.hungryhowies.com/'
+}, {
+    name: 'Jimmy Johns',
+    lat: 41.46749,
+    lng: -87.04097,
+    link: 'https://www.jimmyjohns.com/'
+}, {
+    name: 'CVS Pharmacy',
+    lat: 41.45974,
+    lng: -87.06254,
+    link: 'https://www.cvs.com/'
+}];
 
-];
+// Add a marker for each building
+var markers = {};
+for (var i = 0; i < buildings.length; i++) {
+    var building = buildings[i];
+    markers[building.name] = L.marker([building.lat, building.lng]).addTo(map);
+}
+
+// Define the function to center the map on a specific building
+function centerMap(buildingName) {
+    map.panTo(markers[buildingName].getLatLng());
+}
---
+++ markers.js
@@ -0,0 +1,52 @@
+var markers = [];
+for (var i = 0; i < buildings.length; i++) {
+    var building = buildings[i];
+    var marker = L.marker([building.lat, building.lng]).addTo(map);
+    marker.bindPopup(`<b>${building.name}</b><br>
+                <a href="https://example.com" target="_blank">View inside</a>`);
+    markers.push(marker);
+}
+
+function showBuildingMarker(name) {
+    // loop through the buildings array to find a match for the provided building name
+    for (var i = 0; i < buildings.length; i++) {
+        if (buildings[i].name === name) {
+            var latLng = [buildings[i].lat, buildings[i].lng];
+            // center the map to the location of that building
+            map.setView(latLng, 17);
+            var marker = L.marker(latLng).addTo(map);
+            var popupContent = `<div class='building-name'>${buildings[i].name}</div>
+            <a href="${buildings[i].link}" target="_blank" class='building-link'>View inside</a>`;
+            marker.bindPopup(popupContent);
+            markers.push(marker);
+            marker.openPopup();
+            break;
+        }
+    }
+}
+
+// marker variables for each location
+var Christopher = L.marker([41.46268, -87.04299]).addTo(map);
+var Harre = L.marker([41.46421, -87.04226]).addTo(map);
+var Weseman = L.marker([41.46154, -87.05325]).addTo(map);
+var Neils = L.marker([41.46482, -87.04059]).addTo(map);
+
+// Define the function to center the map on a specific location
+function centerMap(locationId) {
+    switch (locationId) {
+        case 'Christopher Center':
+            map.panTo(Christopher.getLatLng());
+            break;
+        case 'Harre Union':
+            map.panTo(Harre.getLatLng());
+            break;
+        case 'Neils Science Center':
+            map.panTo(Neils.getLatLng());
+            break;
+        case 'Weseman Hall':
+            map.panTo(Weseman.getLatLng());
+            break;
+        default:
+            break;
+    }
+}
