1a9137123bd8f90dd48a2340080963806abe3fd6
jklayhew <justin.layhew@valpo.edu>
2023-01-17 17:12:44 -0600

    added buildings

--- buildings.js
+++ buildings.js
@@ -9,6 +9,58 @@ var buildings = [{
     lat: 41.46421,
     lng: -87.04226
 }, {
+    name: 'Gellersen Center',
+    lat: 41.46271,
+    lng: -87.03799
+}, {
+    name: 'Fites Center',
+    lat: 41.4623,
+    lng: -87.03775
+}, {
+    name: 'Meier Hall',
+    lat: 41.4622,
+    lng: -87.0387
+}, {
+    name: 'Center for the Sciences',
+    lat: 41.46241,
+    lng: -87.03952
+}, {
+    name: 'Kretzmann Hall',
+    lat: 41.46219,
+    lng: -87.04027
+}, {
+    name: 'Helge Center',
+    lat: 41.46273,
+    lng: -87.0419
+}, {
+    name: 'Center for the Arts',
+    lat: 41.46397,
+    lng: -87.04003
+}, {
+    name: 'Schnabel Hall',
+    lat: 41.46387,
+    lng: -87.03834
+}, {
+    name: 'Kallay Christopher Hall',
+    lat: 41.46413,
+    lng: -87.03859
+}, {
+    name: 'Urschel Hall',
+    lat: 41.46411,
+    lng: -87.0376
+}, {
+    name: 'College of Arts and Sciences',
+    lat: 41.46269,
+    lng: -87.04386
+}, {
+    name: 'Mueller Hall',
+    lat: 41.46229,
+    lng: -87.04493
+}, {
+    name: 'Duesenberg Welcome Center',
+    lat: 41.46123,
+    lng: -87.04116
+}, {
     name: 'Weseman Hall',
     lat: 41.46154,
     lng: -87.05325,
