73609abc420ec29b7a8e38a27b6a7d08b2e0263c
jklayhew <83787420+jklayhew@users.noreply.github.com>
2023-01-21 00:32:24 -0500

    made navbar on a test page

--- checkbox.js
+++ checkbox.js
@@ -10,8 +10,7 @@ checkboxes.forEach(function(checkbox) {
         buildings.forEach(function(building) {
             if (selectedBuildings.includes(building.name)) {
                 showMarker(building.name);
-            }
-            else {
+            } else {
                 hideBuildingMarker(building.name);
                 map.closePopup();
             }
@@ -83,4 +82,33 @@ deselectAllForm2.addEventListener("change", function() {
             checkbox.dispatchEvent(new Event('change'));
         });
     }
+});
+
+
+
+// ********************************************************************
+
+// This is the function for the search icon
+
+const nav = document.querySelector(".nav"),
+    searchIcon = document.querySelector("#searchIcon"),
+    navOpenBtn = document.querySelector(".navOpenBtn"),
+    navCloseBtn = document.querySelector(".navCloseBtn");
+
+searchIcon.addEventListener("click", () => {
+    nav.classList.toggle("openSearch");
+    nav.classList.remove("openNav");
+    if (nav.classList.contains("openSearch")) {
+        return searchIcon.classList.replace("uil-search", "uil-times");
+    }
+    searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+navOpenBtn.addEventListener("click", () => {
+    nav.classList.add("openNav");
+    nav.classList.remove("openSearch");
+    searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+    nav.classList.remove("openNav");
 });
