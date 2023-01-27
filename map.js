5259190d21ef4c82a5bf65879cc0c9f675a13684
jklayhew <justin.layhew@valpo.edu>
2023-01-26 20:14:56 -0600

    cleaned up some code

--- map.js
+++ map.js
@@ -5,13 +5,3 @@ var map = L.map('map').setView([41.4628, -87.0442], 16);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(map);
-
-
-
-
-function toggleCategory(event) {
-    var category = event.target.parentNode;
-    var content = category.nextElementSibling;
-    content.style.display = content.style.display === "block" ? "none" : "block";
-    category.classList.toggle("active");
-}
