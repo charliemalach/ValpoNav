ed977b3b16d147d9105c686db3c10feef14e020b
Charlie <87348126+charliemalach@users.noreply.github.com>
2023-01-30 11:47:06 -0600

    updates
    - added sidebar categories
    - fixed sidebar padding

--- js/markers.js
+++ js/markers.js
@@ -10,6 +10,8 @@ for (var i = 0; i < buildings.length; i++) {
     markers.push(marker);   
 }
 
+
+
 function showBuildingMarker(name) {
     // loop through the buildings array to find a match for the provided building name
     for (var i = 0; i < buildings.length; i++) {
@@ -47,3 +49,13 @@ function showMarker(name) {
     }
 }
 
+var greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  })
+  
+  L.marker([41.46811, -87.0275], {icon: greenIcon}).addTo(map);
