1f2ac218e0699dbdcc760dd4a41b19b1ba4eb430
Charlie <87348126+charliemalach@users.noreply.github.com>
2023-02-01 10:29:41 -0600

    got 'wiki' feature working
    each building now has a 'wiki' like page

--- js/buildings.js
+++ js/buildings.js
@@ -7,112 +7,112 @@ var buildings = [
         name: 'Center for the Arts',
         lat: 41.46397,
         lng: -87.04003,
-        link: 'https://www.example1.com'
+        link: './building.html?buildingIndex=',
     }, {
         name: 'Center for the Sciences',
         lat: 41.46241,
         lng: -87.03952,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'Christopher Center',
         lat: 41.46268,
         lng: -87.04299,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'College of Arts and Sciences',
         lat: 41.46269,
         lng: -87.04386,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'Duesenberg Welcome Center',
         lat: 41.46123,
         lng: -87.04116,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'Fites Center',
         lat: 41.4623,
         lng: -87.03775,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'Gellersen Center',
         lat: 41.46271,
         lng: -87.03799,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, { 
         name: 'Harre Union',
         lat: 41.46421,
         lng: -87.04226,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'Helge Center',
         lat: 41.46273,
         lng: -87.0419,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'Kallay Christopher Hall',
         lat: 41.46413,
         lng: -87.03859,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'Kretzmann Hall',
         lat: 41.46219,
         lng: -87.04027,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     },{
         name: 'Meier Hall',
         lat: 41.4622,
         lng: -87.0387,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'Mueller Hall',
         lat: 41.46229,
         lng: -87.04493,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'Neils Science Center',
         lat: 41.46482,
         lng: -87.04059,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'Schnabel Hall',
         lat: 41.46387,
         lng: -87.03834,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     },  {
         name: 'Urschel Hall',
         lat: 41.46411,
         lng: -87.0376,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     },    {
         name: 'Weseman Hall',
         lat: 41.46154,
         lng: -87.05325,
-        link: 'https://www.example1.com'
+        link: './test.html?buildingIndex=',
     }, {
         name: 'CVS Pharmacy',
         lat: 41.45974,
         lng: -87.06254,
-        link: 'https://www.cvs.com/'
+        link: 'https://www.cvs.com/',
     },  {
         name: 'Dominos Pizza',
         lat: 41.46656,
         lng: -87.03962,
-        link: 'https://www.dominos.com/en/'
+        link: 'https://www.dominos.com/en/',
     }, {
         name: 'Hungry Howies',
         lat: 41.46856,
         lng: -87.0468,
-        link: 'https://www.hungryhowies.com/'
+        link: 'https://www.hungryhowies.com/',
     }, {
         name: 'Jimmy Johns',
         lat: 41.46749,
         lng: -87.04097,
-        link: 'https://www.jimmyjohns.com/'
+        link: 'https://www.jimmyjohns.com/',
     }, {
         name: 'Panera Bread',
         lat: 41.46811,
         lng: -87.0275,
-        link: 'https://www.panerabread.com/en-us/home.html'
+        link: 'https://www.panerabread.com/en-us/home.html',
     } ]; 
     
     function getFields(input, field) {
@@ -233,3 +233,4 @@ for (var i=0; i < buildings.length;++i){
 }
 var my_list=document.getElementById("buildings");
 my_list.innerHTML = str;
+
--- js/markers.js
+++ js/markers.js
@@ -3,7 +3,7 @@ for (var i = 0; i < buildings.length; i++) {
     var building = buildings[i];
     var marker = L.marker([building.lat, building.lng]).addTo(map);
     marker.bindPopup(`<b>${building.name}</b><br>
-    <a href="${buildings[i].link}" target="_blank">View inside</a>`);
+    <a href="building.html?buildingIndex=${buildings.indexOf(building)}" target="_blank">View inside</a>`);
     marker.addEventListener("click", function (e) {
         map.panTo(this.getLatLng());
     })
@@ -48,6 +48,8 @@ function showMarker(name) {
         }
     }
 }
+
+
 
 var greenIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
