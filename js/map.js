f7d7b5d77db728d294e06ee0814e46e38b475c82
jklayhew <justin.layhew@valpo.edu>
2023-02-01 21:00:14 -0600

    added toolbar

--- js/map.js
+++ js/map.js
@@ -1,6 +1,8 @@
 //initialize the map
 var map = L.map('map').setView([41.4628, -87.0442], 16);
 
+L.control.navbar().addTo(map);
+
 // Add a tile layer to the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
