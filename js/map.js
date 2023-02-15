677953b9546456acf7af2f3e9a9004fbda467ff4
jklayhew <justin.layhew@valpo.edu>
2023-02-15 10:40:24 -0600

    removed watermark

--- js/map.js
+++ js/map.js
@@ -5,5 +5,5 @@ new L.Control.NavBar().addTo(map);
 
 // Add a tile layer to the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+
 }).addTo(map);
